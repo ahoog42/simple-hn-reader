56d1982742ed117f2b7b3372ff9e8b62bc23e058
Andrew Hoog <ahoog42@gmail.com>
2023-05-31 15:58:57 -0500

    inital code structure from Xcode new project

---
+++ simple-hn-reader/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  simple-hn-reader
+//
+//  Created by Hiro Protagonist on 5/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ simple-hn-reader/simple_hn_readerApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_hn_readerApp.swift
+//  simple-hn-reader
+//
+//  Created by Hiro Protagonist on 5/31/23.
+//
+
+import SwiftUI
+
+@main
+struct simple_hn_readerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
