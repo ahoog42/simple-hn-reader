0bf4f473504bafe1843ec0571f52f8cd8094719a
Andrew Hoog <ahoog42@gmail.com>
2023-06-01 16:52:41 -0500

    init Firebase

--- simple-hn-reader/simple_hn_readerApp.swift
+++ simple-hn-reader/simple_hn_readerApp.swift
@@ -6,9 +6,20 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 @main
-struct simple_hn_readerApp: App {
+struct hn_test_001App: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
