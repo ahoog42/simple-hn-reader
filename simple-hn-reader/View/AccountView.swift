69aa80e4133ab2ee4d5eead4efd2fc2053724117
Andrew Hoog <ahoog42@gmail.com>
2023-06-01 16:53:20 -0500

    update hellow world text

---
+++ simple-hn-reader/View/AccountView.swift
@@ -0,0 +1,20 @@
+//
+//  AccountView.swift
+//  simple-hn-reader
+//
+//  Created by Hiro Protagonist on 6/1/23.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        Text("Account")
+    }
+}
+
+struct AccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountView()
+    }
+}
---
+++ simple-hn-reader/View/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  simple-hn-reader
+//
+//  Created by Hiro Protagonist on 6/1/23.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("App Settings")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
