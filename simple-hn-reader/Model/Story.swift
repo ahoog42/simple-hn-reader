237b349224c04b273fc553a40bf9a440e0237f69
Andrew Hoog <ahoog42@gmail.com>
2023-06-01 16:52:58 -0500

    create model for HN story

---
+++ simple-hn-reader/Model/Story.swift
@@ -0,0 +1,42 @@
+//
+//  Story.swift
+//  simple-hn-reader
+//
+//  Created by Hiro Protagonist on 6/1/23.
+//
+
+import Foundation
+
+// create a Codable struct for a Story
+// the struct should have the following properties:
+// 1. id: Int
+// 2. title: String
+// 3. url: String
+// 4. by: String
+// 5. time: Int
+// 6. score: Int
+// 7. descendants: Int
+// 8. kids: [Int]
+struct Story: Codable {
+    let id: Int
+    let title: String
+    let type: String
+    let url: String
+    let by: String
+    let time: Int
+    let score: Int
+    let descendants: Int
+    let kids: [Int]
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case type
+        case url
+        case by
+        case time
+        case score
+        case descendants
+        case kids
+    }
+}
