d5e6e81914f1413c810836dce253d8e1fccd69ac
Andrew Hoog <ahoog42@gmail.com>
2023-06-01 16:52:10 -0500

    add main bottom nav

--- simple-hn-reader/ContentView.swift
+++ simple-hn-reader/ContentView.swift
@@ -9,13 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        // create a TabView with a NavigationStack
+        // there should be four views in the TabView
+        // 1. NewsView
+        // 2. SearchView
+        // 3. AccountView
+        // 4. SettingsView
+        TabView {
+            NavigationView {
+                NewsView()
+            }
+            .tabItem {
+                Image(systemName: "house")
+                Text("Top News")
+            }
+            NavigationView {
+                SearchView()
+            }
+            .tabItem {
+                Image(systemName: "magnifyingglass")
+                Text("Search")
+            }
+            NavigationView {
+                AccountView()
+            }
+            .tabItem {
+                Image(systemName: "person")
+                Text("Account")
+            }
+            NavigationView {
+                SettingsView()
+            }
+            .tabItem {
+                Image(systemName: "gear")
+                Text("Settings")
+            }
         }
-        .padding()
+
     }
 }
 
