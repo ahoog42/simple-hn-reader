dc87785249785b2e7c8d4cf4f0ac11d5fd2dc06f
Andrew Hoog <ahoog42@gmail.com>
2023-06-03 13:37:13 -0500

    refactor fetch code out of init

--- simple-hn-reader/ModelView/StoriesModelView.swift
+++ simple-hn-reader/ModelView/StoriesModelView.swift
@@ -15,28 +15,32 @@ class StoriesModelView: ObservableObject {
     @Published var stories = [Story]()
     
     init() {
+        fetchStories()   
+    }
+    
+    func fetchStories() {
         // connect to HackerNews API with Firebase Database
         let db = Database.database(url: "https://hacker-news.firebaseio.com/").reference().child("v0").child("topstories")
         // pull top 500 stories
-        db.queryLimited(toFirst: 50).observeSingleEvent(of: .value) { snapshot, error in
+        db.queryLimited(toFirst: 50).getData(completion: { error, snapshot in
             if let error = error {
                 print("Error: \(error)")
                 return
             }
             // loop through the first 500 stories
-            for child in snapshot.children {
+            for child in snapshot!.children {
                 // get the story id
                 let snap = child as! DataSnapshot
                 let value = snap.value as! Int
                 // get the story details
                 let db = Database.database(url: "https://hacker-news.firebaseio.com/").reference().child("v0").child("item").child("\(value)")
-                db.observeSingleEvent(of: .value) { snapshot, error in
+                db.getData(completion: { error, snapshot in
                     // if there is an error, print it out and return
                     if let error = error {
                         print("Error: \(error)")
                         return
                     }
-                    let value = snapshot.value as! [String: Any]
+                    let value = snapshot?.value as! [String: Any]
                     if let id = value["id"] as? Int {
                         let title = value["title"] as? String ?? ""
                         let type = value["type"] as? String ?? ""
@@ -53,11 +57,10 @@ class StoriesModelView: ObservableObject {
                     } else {
                         // handle the case where id is missing
                         print("Error: Invalid story data. Missing id: \(value)")
-                    }                
-                }
+                    }
+                })
             }
-        }
-        
+        })
     }
     
 }
