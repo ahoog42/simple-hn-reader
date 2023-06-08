364ad3adf7f954173b2d82347a6763d9a754107c
Andrew Hoog <ahoog42@gmail.com>
2023-06-08 08:13:50 -0500

    awful search results view

--- simple-hn-reader/View/SearchView.swift
+++ simple-hn-reader/View/SearchView.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct SearchView: View {
+    @StateObject var searchViewModel = SearchViewModel()
+
     var body: some View {
-        Text("Search Results")
+        // create a NavigationView with a textfield in the navigation bar
+        // the textfield should bind to the searchViewModel.searchTerm property
+        // the NavigationView should have a title of "Search"
+        // the NavigationView should have a List embedded
+        // the List should contain a ForEach loop that iterates over the searchViewModel.searchResults property
+        // the ForEach loop should create a StoryOverview for each story
+        NavigationView {
+            VStack {
+                TextField("Search", text: $searchViewModel.searchTerm)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(10)
+                // add a search button to the navigation bar that calls the searchViewModel.search() method
+                // use the magnifyingglass icon for the button
+                Button(action: {
+                    searchViewModel.search()
+                }, label: {
+                    Image(systemName: "magnifyingglass")
+                })
+                List(searchViewModel.searchResults) { story in
+                        Text(story.title ?? String(story.id))
+                        Divider()
+                }
+            }
+        }
     }
 }
 
